8bb2c393fbea86e0c3acdce584291959335ea1b7
Igor Grosu <igrgro@gmail.com>
2022-12-08 19:48:56 +0200

    removed frontend application

--- demo2/src/main/java/com/devolution/demo/controller/HomeController.java
+++ demo2/src/main/java/com/devolution/demo/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.devolution.frontend.controller;
+package com.devolution.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
